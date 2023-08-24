9c1e2afc00efb4f72f11f3bd0e48e5c3ff52ce62
Kilgamesh <notkilgamesh@gmail.com>
2023-08-23 23:50:17 -0400

    ver1.0 Live

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Sketch from './components/Sketch';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Walkman</h1>
+			
+			<header>
+				<p className="header">
+					Walkman generates a 2D{" "}
+					<a href="https://en.wikipedia.org/wiki/Random_walk" target="_blank" rel="noopener noreferrer">
+						random walk
+					</a>{" "}
+					using your input as a seed for the random function that utilizes the algorithm.
+				</p>
+
+				<p>
+					I would love if you shared your results with me on Discord{" "}
+					<a href="https://discord.gg/5Wj3VdRtRQ" target="_blank" rel="noopener noreferrer">
+						@kilgamesh
+					</a>!
+				</p>
+			</header>
+
+			<Sketch />
+
+			<footer>
+				<p>
+					Coded with <span role="img" aria-label="heart">❤️</span> by{" "}
+					<a href="https://kilgamesh.tech" target="_blank" rel="noopener noreferrer">
+						KJ
+					</a>
+				</p>
+			</footer>
     </div>
   );
 }
---
+++ src/components/Sketch.js
@@ -0,0 +1,161 @@
+import { createRef, useEffect } from "react";
+import p5 from "p5";
+
+function Sketch() {
+	const myRef = createRef();
+
+	function sketch(p) {
+		let width = Math.min(p.windowWidth / 1.2, 600);
+		let height = width;
+
+		// Point in center of canvas
+		let x = width / 2;
+		let y = height / 2;
+
+		// Walk variables
+		let stepSize = 1;
+		let numberOfPoints = 125000;
+
+		let name = "";
+		let seed = 0;
+
+		let navigationJump = 50;
+
+		/* Canvas UI */
+
+		// Form Fields
+		let userInput = p.createDiv();
+		let nameField = p.createInput();
+		nameField.attribute("placeholder", "Solid Snake");
+		let generateButton = p
+			.createButton("Generate Walk")
+			.class("btn generate");
+		
+		let walkDisplay = p.createDiv();
+
+		// Canvas Navigation
+		let navigation = p.createDiv().class("navigation");
+		
+		let upButton = p.createButton("Up").class("btn");
+		let downButton = p.createButton("Down").class("btn");
+		let leftButton = p.createButton("Left").class("btn");
+		let rightButton = p.createButton("Right").class("btn");
+
+		let reverse = p.createDiv();
+		let reverseButton = p.createButton("Invert Controls").class("btn reverse");
+
+		/* p5 Setup */
+		p.setup = () => {
+			userInput.child(nameField);
+			userInput.child(generateButton);
+
+			generateButton.mousePressed(convertNameToSeed);
+			walkDisplay.child(p.createCanvas(width, height));
+
+			reverse.child(reverseButton);
+			reverseButton.mousePressed(reverseNavigationControls);
+
+			navigation.child(upButton);
+			navigation.child(downButton);
+			navigation.child(leftButton);
+			navigation.child(rightButton);
+
+			upButton.mousePressed(navigateUp);
+			downButton.mousePressed(navigateDown);
+			leftButton.mousePressed(navigateLeft);
+			rightButton.mousePressed(navigateRight);
+
+			p.pixelDensity(1);
+		}
+
+		function convertNameToSeed() {
+			let accumulator = 0;
+			name = nameField.value();
+			for (let i = 0; i < name.length; i++) {
+				accumulator += name.charCodeAt(i);
+			}
+
+			seed = accumulator;
+			p.redraw();
+		}
+
+		function navigateUp() {
+			p.translate(0, +navigationJump);
+			p.draw();
+		}
+		function navigateDown() {
+			p.translate(0, -navigationJump);
+			p.draw();
+		}
+		function navigateLeft() {
+			p.translate(+navigationJump, 0);
+			p.draw();
+		}
+		function navigateRight() {
+			p.translate(-navigationJump, 0);
+			p.draw();
+		}
+
+		function reverseNavigationControls() {
+			navigationJump += -1;
+		}
+
+		function staticRandomWalk() {
+			x = width / 2;
+			y = height / 2;
+
+			p.background(229);
+			p.randomSeed(seed);
+
+			for (let i = 0; i < numberOfPoints; i++) {
+				let color = p.color(0);
+
+				p.stroke(color);
+				p.strokeWeight(stepSize);
+
+				p.noSmooth();
+				p.point(x, y);
+
+				let r = Math.floor(p.random() * 4);
+
+				switch (r) {
+					case 0:
+						x += stepSize;
+            break;
+          case 1:
+            x -= stepSize;
+            break;
+          case 2:
+            y += stepSize;
+            break;
+          case 3:
+            y -= stepSize;
+            break;
+          default:
+            break;
+        }
+			};
+			p.noLoop();
+		}
+		
+		p.windowResized = () => {
+			if (p.windowWidth < 600) {
+				p.resizeCanvas(p.windowWidth / 1.2, p.windowWidth / 1.2);
+			} else {
+				p.resizeCanvas(600, 600);
+			}
+		};
+
+		p.draw = () => {
+			staticRandomWalk();
+		};
+	}
+
+	useEffect(() => {
+		new p5(sketch, myRef.current);
+	}, [myRef]);
+
+	return <div className="sketch" ref={myRef}></div>;
+}
+
+export default Sketch;
